a816b0b49572817983419be0b9fbc964dd45aba4
carlosalberto900 <carlos.alberto.900@gmail.com>
2025-06-09 18:28:14 -0300

    Update pensao_morte.py

--- pensao_morte.py
+++ pensao_morte.py
@@ -116,7 +116,7 @@ def prazo_pensao_conjuge_companheira(data_do_obito_convertida):
                 tempo = "por 20 anos"
             if idade_autor >= 45:
                 tempo = "de forma vitalícia"
-return tempo_casamento_uniao, carencia_instituidor, tempo
+    return tempo_casamento_uniao, carencia_instituidor, tempo
 
 fundamento_questao = st.radio(
 "**Relatório e fundamentação jurídica**\n\n"
